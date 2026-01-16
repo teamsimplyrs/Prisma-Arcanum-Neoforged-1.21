2cb64706726b70efa57da53403f9641bd1026577
Shlok <savefailedmc@gmail.com>
2026-01-17 03:05:39 +0530

    fix compiler errors

--- src/main/java/com/teamsimplyrs/prismaarcanum/entity/client/projectile/ManaPelletModel.java
+++ src/main/java/com/teamsimplyrs/prismaarcanum/entity/client/projectile/ManaPelletModel.java
@@ -14,7 +14,7 @@ import net.minecraft.resources.ResourceLocation;
 
 public class ManaPelletModel extends EntityModel<ManaPelletProjectile> {
     // This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
-    public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(PrismaArcanum.MOD_ID, ManaPellet.spellID), "main");
+    public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(PrismaArcanum.MOD_ID, ManaPellet.SPELL_ID), "main");
     private final ModelPart main_part;
 
     public ManaPelletModel(ModelPart root) {
